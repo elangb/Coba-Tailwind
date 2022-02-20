b4f47cbd3dc20a1289cff292c3878469813a3c21
elangb <duasanga@gmail.com>
2022-02-21 00:05:52 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        "toko-dongker": "#242A45",
+        "toko-red": "#ff6633",
+        "toko-blue": "#4444A5",
+        "toko-grey": "#9194A2",
+        "toko-grey2": "#ECEEEE",
+        "toko-white": "#F7F7F7",
+        "toko-green": "#15bc15",
+      },
+    },
+    fontFamily: {
+      OpenSans: ["Open Sans", "sans-serif"],
+      Poppins: ["Poppins", "sans-serif"],
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      medium: 500,
+      bold: 700,
+      'extra-bold': 800,
+      black: 900,
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }
+    }
+  },
+  plugins: [],
+}
